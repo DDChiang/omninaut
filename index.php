1820ac22a8b788cf53a7733644a2f10905e20682
Deedee Chiang <DDChiang@users.noreply.github.com>
2014-09-21 18:11:57 -0700

    changed maximum-scale viewport and margin-top for .panel

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <head>
     <link rel="stylesheet" type="text/css" href="home_style.css">
 	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400' rel='stylesheet' type='text/css'>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=3.0" />
 	<title>Omninaut</title>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   </head>
@@ -38,37 +38,45 @@
 	<div class="panel" id="odysey">
 	  <div class="hero_title">ODYSEY</div>
 	  <div class="go_link"><a href="sites/odysey/index.html">Click to visit site</a></div>
-	  <div class="box" id="box1" ><img src="home_img/odysey1.png"></div>
-	  <div class="box" id="box2"><img src="home_img/odysey2.png"></div>
-	  <div class="box" id="box3"><img src="home_img/odysey3.png"></div>
-	  <div class="box" id="box4"><img src="home_img/odysey4.png"></div>
+	  <div class="previews_wrapper">
+	    <div class="box" id="box1" ><img src="home_img/odysey1.png"></div>
+	    <div class="box" id="box2"><img src="home_img/odysey2.png"></div>
+	    <div class="box" id="box3"><img src="home_img/odysey3.png"></div>
+	    <div class="box" id="box4"><img src="home_img/odysey4.png"></div>
+      </div>		
 	</div>
 	
 	<div class="panel" id="piefactory">
 	  <div class="hero_title">PIE FACTORY</div>
 	  <div class="go_link"><a href="sites/piefactory/index.html">Click to visit site</a></div>
-	  <div class="box" id="box1" ><img src="home_img/pie1.png"></div>
-	  <div class="box" id="box2"><img src="home_img/pie2.png"></div>
-	  <div class="box" id="box3"><img src="home_img/pie3.png"></div>
-	  <div class="box" id="box4"><img src="home_img/pie4.png"></div>
+	  <div class="previews_wrapper">
+	    <div class="box" id="box1" ><img src="home_img/pie1.png"></div>
+	    <div class="box" id="box2"><img src="home_img/pie2.png"></div>
+	    <div class="box" id="box3"><img src="home_img/pie3.png"></div>
+	    <div class="box" id="box4"><img src="home_img/pie4.png"></div>
+	  </div>	
 	</div>
 	
 	<div class="panel" id="stgrammercy">
 	  <div class="hero_title">St. GRAMMERCY</div>
 	  <div class="go_link"><a href="http://stgrammercy.com">Click to visit site</a></div>
-	  <div class="box" id="box1" ><img src="home_img/stgram1.png"></div>
-	  <div class="box" id="box2"><img src="home_img/stgram2.png"></div>
-	  <div class="box" id="box3"><img src="home_img/stgram3.png"></div>
-	  <div class="box" id="box4"><img src="home_img/stgram4.png"></div>
+	  <div class="previews_wrapper">
+	    <div class="box" id="box1" ><img src="home_img/stgram1.png"></div>
+	    <div class="box" id="box2"><img src="home_img/stgram2.png"></div>
+	    <div class="box" id="box3"><img src="home_img/stgram3.png"></div>
+	    <div class="box" id="box4"><img src="home_img/stgram4.png"></div>
+	  </div>
 	</div>
 	
 	<div class="panel" id="oaktree">
 	  <div class="hero_title">OAKTREE</div>
 	  <div class="go_link"><a href="sites/oaktree/index.php">Click to visit site</a></div>
-	  <div class="box" id="box1" ><img src="home_img/oaktree1.png"></div>
-	  <div class="box" id="box2"><img src="home_img/oaktree2.png"></div>
-	  <div class="box" id="box3"><img src="home_img/oaktree3.png"></div>
-	  <div class="box" id="box4"><img src="home_img/oaktree4.png"></div>
+	  <div class="previews_wrapper"> 
+	    <div class="box" id="box1" ><img src="home_img/oaktree1.png"></div>
+	    <div class="box" id="box2"><img src="home_img/oaktree2.png"></div>
+	    <div class="box" id="box3"><img src="home_img/oaktree3.png"></div>
+	    <div class="box" id="box4"><img src="home_img/oaktree4.png"></div>
+	  </div>
 	</div>
 	
 	<div class="work_icon"><a href="#"><img src="home_img/work.png"></a></div>
